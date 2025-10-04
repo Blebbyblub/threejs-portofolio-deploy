22791b7095b1d7fac9f8b903c5f479311d2456d9
Blebbyblub <renaltsalle@gmail.com>
2025-10-05 00:46:09 +0700

    update

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText(' raenault.bandaso@binus.ac.id');
     setHasCopied(true);
 
     setTimeout(() => {
@@ -24,9 +24,10 @@ const About = () => {
 
             <div>
               <p className="grid-headtext">Hi, I’m Raenault Maxine Salle Bandaso</p>
-              <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+              <p className="grid-subtext text-justify">
+                Computer Science student at <strong> Bina Nusantara University</strong> specializing in{' '}
+                <strong>Intelligent Systems</strong>. I’m passionate about blending <strong>AI technologies</strong>{' '}
+                with <strong> user-centered app design</strong> to create meaningful, interactive solutions.
               </p>
             </div>
           </div>
@@ -38,16 +39,22 @@ const About = () => {
 
             <div>
               <p className="grid-headtext">Tech Stack</p>
-              <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+              <br />
+              <p className="grid-subtext text-justify">
+                <ul className="list-disc list-outside pl-3 text-base text-gray-300 space-y-1">
+                  <li>Frontend: HTML, CSS, JavaScript, Tailwind, Figma</li>
+                  <li>Backend: PHP, Node.js, MySQL</li>
+                  <li>AI/ML: Python, TensorFlow, XGBoost, YOLO</li>
+                  <li>AR/3D: Unity, Vuforia, C#</li>
+                  <li>Tools: GitHub, Agile, Usability Testing</li>
+                </ul>
               </p>
             </div>
           </div>
         </div>
 
         <div className="col-span-1 xl:row-span-4">
-          <div className="grid-container">
+          <div className="grid-container flex flex-col justify-center items-center">
             <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
               <Globe
                 height={326}
@@ -63,28 +70,34 @@ const About = () => {
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <p className="grid-subtext text-justify">
+                I&apos;m based in Jakarta, Indonesia and open to remote work worldwide as well as offline work in
+                Jakarta.
+              </p>
             </div>
           </div>
         </div>
 
         <div className="xl:col-span-2 xl:row-span-3">
-          <div className="grid-container">
+          <div className="grid-container flex flex-col justify-center items-center">
             <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
 
             <div>
               <p className="grid-headtext">My Passion for Coding</p>
-              <p className="grid-subtext">
-                I love solving problems and building things through code. Programming isn&apos;t just my
-                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              <p className="grid-subtext text-justify">
+                My passion for coding comes from the joy of{' '}
+                <span className="font-semibold">turning ideas into real applications</span>. I enjoy exploring different
+                areas of technology, from <span className="font-semibold">web development </span> to
+                <span className="font-semibold"> interactive experiences</span>, and I’m driven to create solutions that
+                are both
+                <span className="font-semibold"> practical and user-friendly</span>.
               </p>
             </div>
           </div>
         </div>
 
         <div className="xl:col-span-1 xl:row-span-2">
-          <div className="grid-container">
+          <div className="grid-container flex flex-col justify-center items-center">
             <img
               src="assets/grid4.png"
               alt="grid-4"
@@ -95,7 +108,9 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">adrian@jsmastery.pro</p>
+                <p className="lg:text-1xl md:text-xl font-medium text-gray_gradient text-white">
+                  raenault.bandaso@binus.ac.id
+                </p>
               </div>
             </div>
           </div>
